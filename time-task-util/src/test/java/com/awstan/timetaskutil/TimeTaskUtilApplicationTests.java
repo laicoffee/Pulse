1895f6f7179b1d66efe905a7097803829870a8a0
pw7563 <2226009884@qq.com>
2025-01-09 17:33:29 +0800

    搭建

---
+++ time-task-util/src/test/java/com/awstan/timetaskutil/TimeTaskUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awstan.timetaskutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTaskUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
